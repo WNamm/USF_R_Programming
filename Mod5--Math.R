0d4ab0eaf2ef6b89dbb56fcb29dddd581c7152ea
William Namm <WNamm7@gmail.com>
2021-02-15 01:44:46 -0500

    Updated module 5 assignment to have set.seed() function for creating matrix C

--- Mod5--Math.R
+++ Mod5--Math.R
@@ -16,6 +16,9 @@ solve(t(A))
 #Now the determinant of B and its inverse are attempted.
 det(B)
 solve(B)
+#A seed is set for the random number generator.
+#That way, matrix C below has the same elements every time the script runs.
+set.seed(735)
 #A new matrix is created from random integers, and then its determinant and inverse are found.
 C <- matrix(floor(runif(100, 1, 100)), nrow=10)
 C
