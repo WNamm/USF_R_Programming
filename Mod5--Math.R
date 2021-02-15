ec42674b0f87d1c434c9f4ab7e87941e88e86aba
William Namm <WNamm7@gmail.com>
2021-02-14 23:29:18 -0500

    Uploading module 5 assignment.

---
+++ Mod5--Math.R
@@ -0,0 +1,25 @@
+#Module 5 Assignment: Doing Math and Statistics
+#This script finds the determinants and inverses of two example matrices.
+#Since these matrices do not have inverses, another matrix is tried afterward.
+#First, the matrices are defined.
+A <- matrix(1:100, nrow=10)
+B <- matrix(1:1000, nrow=10)
+A
+B
+#Starting with A, the determinant is found and the inverse is attempted.
+det(A)
+solve(A)
+#Matrices with determinants of 0 have no inverse.  So, the transpose of A is tried instead.
+t(A)
+det(t(A))
+solve(t(A))
+#Now the determinant of B and its inverse are attempted.
+det(B)
+solve(B)
+#A new matrix is created from random integers, and then its determinant and inverse are found.
+C <- matrix(floor(runif(100, 1, 100)), nrow=10)
+C
+det(C)
+solve(C)
+#The matrices C and its inverse are multiplied to test and see if they make an identity matrix.
+C%*%solve(C)
