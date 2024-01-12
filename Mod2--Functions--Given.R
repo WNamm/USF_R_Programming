bfdb76f75c2283539e5ea79073b77c8b642463bc
William Namm <WNamm7@gmail.com>
2024-01-12 02:18:13 -0500

    Updated Mod2--Functions--Given
    Added to the comment in line 2 of this file to specify that this script is intentionally broken.

--- Mod2--Functions--Given.R
+++ Mod2--Functions--Given.R
@@ -1,5 +1,5 @@
 #Module 2 Assignment: Introduction to Data Types and Functions
-#This is the given code to evaluate.
+#This is the given code to evaluate. It is intentionally broken, and I had to fix it for the assignment.
 assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
 myMean <- function(assignment2) {
   return(sum(assignment)/length(someData))
